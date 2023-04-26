d5e276559c180ae81c275eb49f376aa31fca9f18
agustinhidalgo10 <agustinhidalgocef18@gmail.com>
2023-04-26 13:18:54 -0300

    terminando los entregables

---
+++ desafio-12/src/routes/products.router.js
@@ -0,0 +1,9 @@
+import Router from 'express'
+import { saveController, getAllController } from '../controllers/products.controllers.js'
+
+const ProductsRouter = Router()
+
+ProductsRouter.post('/add', saveController);
+ProductsRouter.get('/get', getAllController);
+
+export default ProductsRouter
